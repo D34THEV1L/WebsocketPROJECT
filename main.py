e1527ab0922a1936055046f8460184b52ce107bb
D34TH_3V1L <87221905+D34THEV1L@users.noreply.github.com>
2023-08-13 00:56:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from structs import discord, websocket
 import asyncio
 
-__TOKEN = "MTA3NTMxMjMwODY0MjkxMDI3OQ.GAspN0.f73nNfF6UgtnUD-bSP6ScpccgOW7P61ImleCFs"
+__TOKEN = "YOUR TOKEN HERE"
 
 asyncio.get_event_loop().create_task(discord.client.start(__TOKEN))
 websocket.init()
